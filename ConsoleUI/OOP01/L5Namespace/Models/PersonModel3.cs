ede148c5abdb2401a56d1d3d94bd6ac2cb338806
aonchicken <aonchicken@gmail.com>
2022-05-18 15:06:48 +0700

    feat: move folder.

--- ConsoleUI/OOP01/L5Namespace/Models/PersonModel3.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ConsoleUI.OOP01.L5Namespace.Models
-{
-    public class PersonModel3
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
